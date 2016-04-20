89214a4ad72790e90ec7a72f30a6a42263ed2e0a
Ajk2rxKamehameha <ajeet.meena@gmail.com>
2016-04-20 20:58:43 +0530

    Added 3 Logins condition on splashActivity. + Minor Ui Changes

--- app/src/main/java/one/thebox/android/activity/SignUpActivity.java
+++ app/src/main/java/one/thebox/android/activity/SignUpActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import one.thebox.android.R;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button submitButton;
+    private EditText nameEditText, nameEmail, localityEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +23,11 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void initViews() {
-        submitButton = (Button) findViewById(R.id.sign_up_button);
+        submitButton = (Button) findViewById(R.id.button_submit);
         submitButton.setOnClickListener(this);
+        nameEditText = (EditText) findViewById(R.id.edit_text_name);
+        nameEmail = (EditText) findViewById(R.id.edit_text_email);
+        localityEditText = (EditText) findViewById(R.id.edit_text_locality);
     }
 
     @Override
--- app/src/main/java/one/thebox/android/activity/SplashActivity.java
+++ app/src/main/java/one/thebox/android/activity/SplashActivity.java
@@ -5,7 +5,9 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import one.thebox.android.Models.User;
 import one.thebox.android.R;
+import one.thebox.android.util.PrefUtils;
 
 /**
  * Created by Ajeet Kumar Meena on 8/10/15.
@@ -21,7 +23,15 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, OnBoardingActivity.class));
+                String token = PrefUtils.getToken(SplashActivity.this);
+                User user = PrefUtils.getUser(SplashActivity.this);
+                if ((user == null || user.getName() == null || user.getName().isEmpty()) && token.isEmpty()) {
+                    startActivity(new Intent(SplashActivity.this, OnBoardingActivity.class));
+                } else if ((user == null || user.getName() == null || user.getName().isEmpty()) && !token.isEmpty()) {
+                    startActivity(new Intent(SplashActivity.this, SignUpActivity.class));
+                } else if(!(user == null || user.getName() == null || user.getName().isEmpty()) && !token.isEmpty()) {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                }
                 finish();
             }
         }, DELAY);
